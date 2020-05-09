b1a035d6b80ed829dbca39e4da0fb90f1739c5a8
Josef Wasinski <josef.wasinskI@gmail.com>
2020-05-09 18:36:18 +0100

    Pointless

--- run.sh
+++
@@ -1,1 +0,0 @@
-nativefier https://roamresearch.com/#/app -i icon.icns --internal-urls "(roamresearch)|(firebaseio)|(firebasestorage)/gi" --hide-window-frame --single-instance --name Roam --inject main.css
